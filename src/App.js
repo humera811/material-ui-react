645c06eb476bdb1640dfec7f6e3d8f01e07cf3e8
humera811 <humera811@gmail.com>
2023-04-17 14:03:26 -0700

    update

--- src/App.js
+++ src/App.js
@@ -1,22 +1,56 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ButtonGroup from '@mui/material/ButtonGroup'
+import Button from '@mui/material/Button'
+import SaveIcon from '@mui/icons-material/Save';
+import DeleteIcon from '@mui/icons-material/Delete';
+import CheckBox from '@mui/icons-material/CheckBox';
+import FormControlLabel from '@mui/material/FormControlLabel';
+
+
+function CheckBoxExample() {
+  const [checked, setChecked] = React.useState(true);
+  const handleChange = (event) => {
+    console.log(event.target.checked);
+    setChecked(event.target.checked);
+  };
+  return (
+    <FormControlLabel
+      control= { 
+      <CheckBox 
+      checked = {checked}
+      onChange = {handleChange}
+      inputprops={{ 'aria-label': 'secondary checkbox' }} 
+    />
+  }
+        label = "Testing Checkbox"
+        
+    />
+  );
+}
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <CheckBoxExample />
+        <ButtonGroup variant='contained' aria-label='outlined primary button group' color='primary'>
+        <Button
+        startIcon = {<SaveIcon />}
         >
-          Learn React
-        </a>
+          Save
+        </Button>
+
+        <Button
+        startIcon = {<DeleteIcon />}
+        >
+          Discard
+        </Button>
+        </ButtonGroup>
+        <img src={logo} className="App-logo" alt="logo" />
+        
       </header>
     </div>
   );
